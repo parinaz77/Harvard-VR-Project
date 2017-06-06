5c381a1f9ddb5dbea90e63a793e21cd75d38cdef
parinaz77 <parinaz77@gmail.com>
2017-06-06 15:08:41 -0700

    edit app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const path = require('path');
-const config = require('./config');
 
 const app = express();
 const indexRoutes = require('./routes/index');
 
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-app.set('trust proxy', true);
 
 app.use(indexRoutes);
 
@@ -16,8 +12,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, () => {
 	console.log('Listening on port 3000!');
 });
-const app = express();
 
-
-const port = process.env.PORT 
-app.listen()
