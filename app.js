eb507375ff0c516870b7d463c0e86a2654b92eae
Tony Nguyen <tntuyen@ucdavis.edu>
2017-06-06 16:48:24 -0700

    add model for image upload

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express');
-const path = require('path');
-const config = require('./config');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var multer = require('multer');
 
 const app = express();
 const indexRoutes = require('./routes/index');
 
-app.set('views', path.join(__dirname, 'views'));
+// const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/harvard-vr";
+
+mongoose.connect(url);
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-app.set('trust proxy', true);
 
 app.use(indexRoutes);
+
+var photoSchema = mongoose.Schema({
+	img: {data: Buffer, contentType: String}
+});
+
+var Photo = mongoose.model('Photo', photoSchema);
+
+app.post('/api/photo', function(req, res) {
+	var newPhoto = new Photo();
+	newPhoto.img.data = fs.readFileSync(req.files.userPhoto.path);
+	newPhoto.img.contentType = 'image/png';
+	newPhoto.save();
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, () => {
 	console.log('Listening on port 3000!');
 });
-
