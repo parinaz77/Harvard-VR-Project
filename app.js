515fdde3dd6cde4bf68f3cf84bf5e6791a7a549f
parinaz77 <parinaz77@gmail.com>
2017-06-06 15:02:52 -0700

    add views

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+
 const app = express();
+const indexRoutes = require('./routes/index');
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.use(indexRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, () => {
+	console.log('Listening on port 3000!');
+});
 
 
-const port = process.env.PORT 
-app.listen()
