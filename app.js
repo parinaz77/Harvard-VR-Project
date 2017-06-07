b5f770497c37c67316a7f569163d253f8d0fcb3f
Tony Nguyen <tntuyen@ucdavis.edu>
2017-06-07 12:49:31 -0700

    add routes for image uploads

--- app.js
+++ app.js
@@ -1,36 +1,79 @@
+const format = require('util').format;
 const express = require('express');
-var fs = require('fs');
-var mongoose = require('mongoose');
+const fs = require('fs');
+const mongoose = require('mongoose');
 var multer = require('multer');
+
+var upload = multer({dest: "./uploads"});
 
 
 const app = express();
 const indexRoutes = require('./routes/index');
 
-// const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
+const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
 // var url = process.env.DATABASEURL || "mongodb://localhost:27017/harvard-vr";
 
 // mongodb://<dbuser>:<dbpassword>@ds113841.mlab.com:13841/harvard-vr
 
 
-mongoose.connect("mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr");
+mongoose.connect(url);
+
+var conn = mongoose.connection;
+
+var gfs;
+
+var Grid = require("gridfs-stream");
+Grid.mongo = mongoose.mongo;
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.use(indexRoutes);
 
-var photoSchema = mongoose.Schema({
-	img: {data: Buffer, contentType: String}
-});
+conn.once("open", function(){
+  gfs = Grid(conn.db);
+  app.get("/", function(req,res){
+    //renders a multipart/form-data form
+    res.render("home");
+  });
 
-var Photo = mongoose.model('Photo', photoSchema);
+  //second parameter is multer middleware.
+  app.post("/", upload.single("avatar"), function(req, res, next){
+    //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
+    var writestream = gfs.createWriteStream({
+      filename: req.file.originalname
+    });
+    //
+    // //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
+    fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
+        .on("err", function(){res.send("Error uploading image")})
+          .pipe(writestream);
+  });
 
-app.post('/api/photo', function(req, res) {
-	var newPhoto = new Photo();
-	newPhoto.img.data = fs.readFileSync(req.files.userPhoto.path);
-	newPhoto.img.contentType = 'image/png';
-	newPhoto.save();
+  // sends the image we saved by filename.
+  app.get("/:filename", function(req, res){
+      var readstream = gfs.createReadStream({filename: req.params.filename});
+      readstream.on("error", function(err){
+        res.send("No image found with that title");
+      });
+      readstream.pipe(res);
+  });
+
+  //delete the image
+  app.get("/delete/:filename", function(req, res){
+    gfs.exist({filename: req.params.filename}, function(err, found){
+      if(err) return res.send("Error occured");
+      if(found){
+        gfs.remove({filename: req.params.filename}, function(err){
+          if(err) return res.send("Error occured");
+          res.send("Image deleted!");
+        });
+      } else{
+        res.send("No image found with that title");
+      }
+    });
+  });
 });
 
 const port = process.env.PORT || 3000;
