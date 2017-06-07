74947330688b9a797fefd6293d2f8451a34b9d99
parinaz77 <parinaz77@gmail.com>
2017-06-06 17:50:21 -0700

    add mongo

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const app = express();
 const indexRoutes = require('./routes/index');
 
 // const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/harvard-vr";
+// var url = process.env.DATABASEURL || "mongodb://localhost:27017/harvard-vr";
+// mongodb://<dbuser>:<dbpassword>@ds113282.mlab.com:13282/heroku_03ks57hf
 
-mongoose.connect(url);
+mongoose.connect('mongodb://parinaz77:password@ds113282.mlab.com:13282/heroku_03ks57hf');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
