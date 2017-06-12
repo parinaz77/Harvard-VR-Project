c2cf631715109751af4df94d3af92b90c0194f3e
Tony Nguyen <tntuyen@ucdavis.edu>
2017-06-11 22:44:40 -0700

    minor change to gcs instance config

--- app.js
+++ app.js
@@ -4,51 +4,26 @@ const mongoose = require('mongoose');
 var request = require('request');
 var fileUpload = require('express-fileupload');
 var methodOverride = require('method-override');
+
 var gcloud = require('google-cloud');
 var gcs = gcloud.storage({
   projectId: 'harvard-vr-169919',
   keyFilename: './harvard-vr-93a42650c36e.json'
 });
-// var bucket = gcs.bucket('harvard-vr')
+var bucket = gcs.bucket('harvard-vr')
 
 const app = express();
 const indexRoutes = require('./routes/index');
 
-// Service Account credentials for server-server interactions
-var gcs = gcloud.storage({
-  projectId: 'harvard-vr-169919',
-  keyFilename: './harvard-vr-93a42650c36e.json'
-});
-
-const clientID = '144708981782-5hht2hv7cve7omnkd09ia3s9jc79btdd.apps.googleusercontent.com';
-const clientSecret = 'rDo72rlRiXovvVl1JBeH0C-g';
-
-// Imports the Google Cloud client library
-const Storage = require('@google-cloud/storage');
-
-// Your Google Cloud Platform project ID
-const projectId = 'YOUR_PROJECT_ID';
-
-// Instantiates a client
-const storage = Storage({
-  projectId: 'harvard-vr-169919'
-});
-
-// The name for the new bucket
-const bucket = 'harvard-vr';
-
-// Creates the new bucket
-storage.createBucket(bucket)
-  .then(() => {
-    console.log(`Bucket ${bucket} created.`);
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
-
-
-// connects to MLAB instance or creates a new database locally if can't connect
 const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
+// const url = "mongodb://parinaz77:password@ds113282.mlab.com:13282/heroku_03ks57hf" || "mongodb://localhost:27017/harvard-vr";
+
+// mongodb://<dbuser>:<dbpassword>@ds113841.mlab.com:13841/harvard-vr
+
+// const url = "mongodb://tonyn4444:password@ds113841.mlab.com:13841/harvard-vr" || "mongodb://localhost:27017/harvard-vr";
+// var url = process.env.DATABASEURL || "mongodb://localhost:27017/harvard-vr";
+// mongodb://<dbuser>:<dbpassword>@ds113282.mlab.com:13282/heroku_03ks57hf
+
 mongoose.connect(url);
 
 
@@ -63,18 +38,17 @@ const collectionSchema = new mongoose.Schema({
 const Collection = mongoose.model('Collection', collectionSchema);
 
 // Collection.create({title: 'vr-research', images: ['test_3.png','test_4.png']});
+// 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(fileUpload());
 app.use(methodOverride('_method'));
+
 app.use(indexRoutes);
 
 var collection = gcs.bucket('harvard-vr');
 
-// ================================================================================
-// ROUTES
-// ================================================================================
-
+// Example request: 'https://www.googleapis.com/storage/v1/b/harvard-vr/o/test.png'
 app.get('/collections', function(req, res) {
 	Collection.find({}, function(err, collections) {
 		if(err) {
@@ -96,13 +70,7 @@ app.get('/test', function(req, res) {
 });
 
 app.post('/upload', function(req, res) {
-	// Downloads image from form element locally
 	let image = req.files.image;
-	image.mv('public/photos/' + req.files.image.name);
-
-	// Uploads image stored locally to cloud storage
-	collection.upload('public/photos/' + req.files.image.name), function(err, file) {
-		console.log('Attempting to upload image');
 	for (var i=0; i < image.length; i++){
 		image[i].mv('public/photos/' + req.files.image[i].name);
 		collection.upload('public/photos/' + req.files.image[i].name), function(err, file) {
@@ -111,8 +79,7 @@ app.post('/upload', function(req, res) {
 			}
 		}
 	}
-
-	// Create a new Collection to save reference of images in cloud
+	// Create a new Collection with information from form in 'collections.ejs'
 	Collection.create({title: req.body.title, description: req.body.description, images: req.files.image}, function(err, collection) {
 		if(err) {
 			console.log(err);
@@ -120,7 +87,7 @@ app.post('/upload', function(req, res) {
 			console.log('Collection created successfully!');
 			res.redirect('/collections');
 		}
-	})
+	});
 });
 
 app.get('/collections/:id/edit', function(req, res) {
@@ -166,7 +133,7 @@ app.put('/collections/:id', function(req, res) {
 	}
 	res.redirect('/collections');
 	console.log('Hit PUT route,', req.files)
-});
+})
 
 
 const port = process.env.PORT || 3000;
