38fa9cd0afed76ee4515f505dee9cbc24e03a69f
parinaz77 <parinaz77@gmail.com>
2017-06-13 13:54:43 -0700

    add vr selector

--- routes/index.js
+++ routes/index.js
@@ -114,6 +114,16 @@ router.get('/collections/:id/vr', function(req, res) {
 	})
 });
 
+router.get('/collections/:id/vr2', function(req, res) {
+	Collection.findById(req.params.id, function(err, collection) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render('vr2', {collection: collection})
+		}
+	})
+});
+
 router.get('/test', function(req,res) {
 	res.render('test');
 });
