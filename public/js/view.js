5c4806e31cd08bde117b619f6f9d5da778a1eba1
parinaz77 <parinaz77@gmail.com>
2017-06-14 17:30:00 -0700

    edit collection card view

--- public/js/view.js
+++ public/js/view.js
@@ -27,14 +27,11 @@ $(document).ready(function() {
 	$('.row-footer').html(
     '<div class="col l6 s12">'+
       '<h5 class="white-text">Contribute to WebVision</h5>'+
-      '<p class="grey-text text-lighten-4">WebVision is an open source project. You can contribute to add more functionality for your study purposes or ask us to help you with that. </p>'+
+      '<p class="grey-text text-lighten-4">WebVision is an open source project. You can contribute to add more functionality for your study purposes or ask us to help you with that. Visit our <a class="grey-text text-lighten-3" href="https://github.com/parinaz77/Harvard-VR-Project">Github repo here</a></p>'+
     '</div>'+
     '<div class="col l4 offset-l2 s12">'+
-      '<h5 class="white-text">Links</h5>'+
+      '<h5 class="white-text">Contributors</h5>'+
       '<ul>'+
-        '<li><a class="grey-text text-lighten-3" href="/">Home</a></li>'+
-        '<li><a class="grey-text text-lighten-3" href="https://github.com/parinaz77/Harvard-VR-Project">Github</a></li>'+
-
         '<li><i class="color-white tiny material-icons">perm_identity</i><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/parinaz-khosraviani/">Parinaz khosraviani</a></li>'+
         '<li><i class="color-white tiny material-icons">perm_identity</i><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/tony-t-nguyen/">Tony Nguyen</a></li>'+
       '</ul>'+
